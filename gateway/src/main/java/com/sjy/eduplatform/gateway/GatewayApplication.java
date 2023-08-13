9383c7f6a5b9c55f439efa94ea8044ae0730f699
hechuansheng <4rfv$RFV>
2023-08-13 18:41:36 +0800

    Initial commit

---
+++ gateway/src/main/java/com/sjy/eduplatform/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.sjy.eduplatform.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : hechuansheng
+ * @date : 2023/8/10 15:35
+ * @since : version-1.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
