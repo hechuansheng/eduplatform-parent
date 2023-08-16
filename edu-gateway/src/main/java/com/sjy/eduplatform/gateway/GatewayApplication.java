2326bf5c3025c098d917ca048d94bfcabaf4b777
hechuansheng <4rfv$RFV>
2023-08-16 14:13:51 +0800

    配置中心初始化

--- edu-gateway/src/main/java/com/sjy/eduplatform/gateway/GatewayApplication.java
+++ edu-gateway/src/main/java/com/sjy/eduplatform/gateway/GatewayApplication.java
@@ -1,12 +1,14 @@
 package com.sjy.eduplatform.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author : hechuansheng
  * @date : 2023/8/14 23:32
  * @since : version-1.0
  */
+@SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
